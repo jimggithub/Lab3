804c6b77de4fb39c1b5ef7fb4a6bd3cdefa055da
FMR <fmresearchpartners@gmail.com>
2017-11-02 22:11:13 -0400

    Initial check-in of lab3 tool

---
+++ jimlab3/src/jimlab3/CampPosanivee.java
@@ -0,0 +1,45 @@
+package jimlab3;
+
+import java.util.Scanner;
+import java.io.*;
+
+/****************************************************************************
+ * CampPosanivee The program will keep track of who is enrolled for the two-week
+ * summer camp. The program uses a binary search tree to maintain the set of
+ * campers enrolled in Camp Posanivee. The program is not case sensitive and
+ * read a text file to process commands. The commands include: H (help), E
+ * (enroll new camper), W (withdraw a camper), D (display the age and gender of
+ * a camper), A (display avg age of campers), L (list all campers), S (print the
+ * number of boy and girl campers), P (display all campers names in preorder), Q
+ * (quit).
+ * 
+ * @author Name
+ * @version 1.0
+ * @date Date
+ * @course MCIS-0503 Summer 2014 Data Structures and Algorithms
+ * @instructor Dr. Jeremy Lanman
+ *****************************************************************************/
+public class CampPosanivee {
+
+	static String name;
+	static String age;
+	static String gender;
+	static Camper camper = null;
+
+	/*************************************************************************
+	 * main The main method is the controlling method for Camp Posanivee.
+	 * Preconditions: must have properly formatted data file. Postconditions: be
+	 * able to create a Queue and BST for data.
+	 * 
+	 * @param args
+	 * @throws IOException
+	 ************************************************************************/
+	public static void main(String[] args) throws IOException {
+
+		// logic here…
+
+		System.out.println("Start of the program");
+
+	}
+
+}
